b1b6dc5953647d62c1356cc8c6ab552edb6354d3
felipe silva <fjdsilv.dev@protonmail.com>
2024-06-03 19:07:57 -0300

    get SingleOrder working

--- src/pages/SingleOrder.jsx
+++ src/pages/SingleOrder.jsx
@@ -3,16 +3,18 @@ import { getOrder } from "../utils/apiRestaurant";
 
 export const loader = async ({ params }) => {
   const id = params.orderId;
-  const order = await getOrder(id)
-  console.log(order)
-
-  return null
+  const order = await getOrder(id);
+  console.log(order);
+  return order
 }
 
 const SingleOrder = () => {
-  useLoaderData();
+  const { id, status, priority, priorityPrice, orderPrice, estimatedDelivery, cart } = useLoaderData();
   return (
-    <div>SingleOrder</div>
+    <div>
+      <h2>Status</h2>
+      
+    </div>
   )
 }
 export default SingleOrder
